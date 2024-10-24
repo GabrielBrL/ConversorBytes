095a81d6b74f09e825e1543fac7f294492b9a196
Gabriel Barros <gabriellourencobarros02@gmail.com>
2024-10-23 21:23:09 -0300

    Adicionando projeto para conversão de bytes em unidade de memória

---
+++ ConversorBytes/ConversorBytes.cs
@@ -0,0 +1,22 @@
+﻿namespace Conversor;
+public static class ConversorBytes
+{
+    public static string Converter<T>(T bytes) where T : struct
+    {
+        string[] unit = new string[] { "B", "KB", "MB", "GB", "TB", "PB" };
+
+        int count = 0;
+
+        if (Double.TryParse(bytes.ToString(), out double bytesDouble))
+        {
+            while (bytesDouble >= 1024)
+            {
+                bytesDouble /= 1024;
+                count = count == unit.Count() - 1 ? count : count + 1;
+            }
+
+            return string.Format("{0} {1}", Math.Round(bytesDouble, 2), unit[count]);
+        }
+        return "Não é possível converter esse valor.";
+    }
+}
---
+++ ConversorBytes/README.md
@@ -0,0 +1,39 @@
+# Conversor to bytes
+
+
+## Following the examples:
+
+```c#
+using Conversor;
+
+var result = ConversorBytes.Converter(float.MaxValue);
+
+Console.Write(result);
+
+256 PB
+```
+
+```c#
+using Conversor;
+
+var result = ConversorBytes.Converter(43242423);
+
+Console.Write(result);
+
+41,24 MB
+```
+## Install
+
+```bash
+  dotnet add package ConversorBytes
+```
+    
+## Author
+
+- [@GabrielBrl](https://github.com/GabrielBrL)
+
+
+## Licence
+
+[MIT](https://choosealicense.com/licenses/mit/)
+
